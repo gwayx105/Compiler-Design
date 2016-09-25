5d64fa014b77d727d12bc13bd8b20b258f5195f7
Sylvester Zowonu <szowonu@hotmail.com>
2016-09-25 11:58:54 -0700

    v1.0

--- Compiler Design/Production.cpp
+++ Compiler Design/Production.cpp
@@ -29,10 +29,8 @@ void Production::loadGrammarList(std::string &lhs, std::string &rhs) {
 void Production::First(std::string lhs[],std::string rhs[]) {
 	std::vector<std::string> tempVar;
 	std::string L_Holder = "";
-	std::string R_Holder = "";
-	std::string inner_L_Holder = "";
-	std::string inner_R_Holder = "";
-	//int loop = 0;
+	//::string R_Holder = "";
+
 	std::map<std::string, std::string> first_map;
 	std::vector <std::string>::iterator i = tempVar.begin();
 	std::string temp = "";
@@ -42,8 +40,26 @@ void Production::First(std::string lhs[],std::string rhs[]) {
 		for (int i = 0;i <= 62;i++) {
 			if (lhs[i].compare(temp)==0) {
 				if (std::any_of(terminal_set.begin(), terminal_set.end(), bind2nd(std::equal_to<std::string>(), rhs[i]))) {
-					tempVar.push_back(rhs[i]);	
+					
+					if (std::any_of(tempVar.begin(), tempVar.end(), bind2nd(std::equal_to<std::string>(), rhs[i]))){
+					
+					}
+					else {
+						tempVar.push_back(rhs[i]);
+					}
+						
 					//first_map.insert(std::pair<std::string, std::string>(lhs[i], rhs[i]));
+					
+				}
+				else if (lhs[i].compare(rhs[i])==0) {
+					//i += 1;
+					if (lhs[i].compare(temp) == 0) {
+						if (std::any_of(terminal_set.begin(), terminal_set.end(), bind2nd(std::equal_to<std::string>(), rhs[i]))) {
+							tempVar.push_back(rhs[i]);
+						}
+						else { temp = rhs[i]; }
+
+					}	
 				}
 				else {
 					auto it = first_map.find(rhs[i]);
@@ -51,17 +67,12 @@ void Production::First(std::string lhs[],std::string rhs[]) {
 						//first_map[rhs[i]] = it->second;
 						//first_map.insert(std::pair<std::string, std::string>(rhs[i], it->second));
 						tempVar.push_back(it->second);
-						//std::cout << it->second<<"\n";
-						//std::cout << it->first << "\n";
-						//system("pause");
-						//continue;
+						
 					}
-			
 					temp = rhs[i];
 				}
-				
+
 			}
-			
 		}
 		//std::cout << "First( " << L_Holder << " ) = { ";
 		temp = "";
@@ -78,15 +89,10 @@ void Production::First(std::string lhs[],std::string rhs[]) {
 	for (auto p : first_map) {
 		std::cout << "First[" << p.first << "] = " << p.second << '\n';
 	}
+	//std::cout<<first_map
 
 }
 
 void Production::Follow() {
 
-}
-std::istream&  operator >> (std::istream& inputStream, Production &testProduction) {
-	return inputStream;
-}
-std::ostream& operator << (std::ostream& outputStream, Production &testProduction) {
-	return outputStream;
 }
